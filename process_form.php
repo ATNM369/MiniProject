eb9292927b87bf42e6a8e52d67ef394c07666c84
Viraj Tharuka Ranathunga <virajtharuka.fb@gmail.com>
2023-09-09 00:02:15 +0530

    email sent successful

--- process_form.php
+++ process_form.php
@@ -1,32 +1,49 @@
 <?php
-
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "geotechhubsusl@gmail.com"; // Replace with the recipient's email address
-    $subject = "Contact Form Submission from $name";
-    $headers = "From: $email";
     $message_body = "Name: $name\nEmail: $email\nMessage:\n$message";
 
-    // Send the email
-    if ( mail ($to, $subject, $message_body, $headers)) {
-        echo "Your message has been sent successfully!";
-    } else {
-        echo "Error sending the message. Please try again later.";
-    }
-}
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    $mail = new PHPMailer(true);
+    
+        try {
+            // SMTP configuration
+            $mail->isSMTP(); 
+            $mail->SMTPAuth = true; 
+            $mail->Host = 'smtp.gmail.com';                                                 
+            $mail->Username = '';                   
+            $mail->Password = '';   
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         
+            $mail->Port = 465; 
+    
+            // Sender and recipient settings
+            $mail->setFrom('no-reply@sas.susl.com', "Student Attendance System");
+            $mail->addReplyTo('no-reply@sas.susl.com', "Student Attendance System");
+            $mail->addAddress($email);
+    
+            // Email content
+            $mail->isHTML(true);                                  
+            $mail->Subject = 'Password Reset Request from Student Attendance System';
+            $email_template = $message_body;
+            $mail->Body = $email_template;
+    
+            // Send email
+            $mail->send();
+            
+            header("Location:contact.php?message_sent_successfully");
+            exit;
+
+
+        } catch (Exception $e) {
+            header("Location:contact.php?showModal=true&status=unsuccess&message=Email not sent! " . $e->getMessage());
+        }
+
 ?>
