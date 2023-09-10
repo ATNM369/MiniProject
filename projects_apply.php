5952b660f6d61a3fdda6803bbe1b1db5adec78cc
ATNM369 <143802706+ATNM369@users.noreply.github.com>
2023-09-10 21:02:28 +0530

    changes

--- projects_apply.php
+++ projects_apply.php
@@ -1,3 +1,16 @@
+<?php 
+    session_start();
+    if(!isset($_SESSION['userName']) ){      
+        header ('Location:index.php');
+        exit;
+    }elseif(isset($_SESSION['role'])){
+        if($_SESSION["role"] !== "admin"){
+            header ('Location:index.php');
+            exit;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
